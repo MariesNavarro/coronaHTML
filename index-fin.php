d65efba58b85cd3a143b13554cdd65ec90998412
Mn (MacBook) <marieshn@gmail.com>
2020-02-27 18:41:21 -0600

    pagina para cierre

---
+++ index-fin.php
@@ -0,0 +1,22 @@
+<?php include 'header.php';?>
+
+<section id="home">
+  <div id="photo" class="backImgCover"></div>
+  <div id="info">
+    <img src="img/gana-fin.svg">
+     <!-- <a href="registro.php" class="btns btRegistro trans5">
+      <div class="color trans5"></div>
+      <p>Registra</p>
+    </a> -->
+    <p style="line-height:1.2; font-size:1.6rem; color:#fff; margin-top: -20px;">CORONA AGRADECE <br> A  TODOS LOS <br>  PARTICIPANTES</p>
+  </div>
+</section>
+
+<script>
+	$(document).ready(function(){
+	  	console.log('ready');
+			$('footer').addClass('home');
+	});
+</script>
+
+<?php include 'footer.php';?>
